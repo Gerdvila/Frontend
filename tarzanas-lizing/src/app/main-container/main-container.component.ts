227debddc2618760b14da8f4c571a0f0cec66060
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-04-10 20:48:53 +0300

    add car leasing calculator html

--- tarzanas-lizing/src/app/main-container/main-container.component.ts
+++ tarzanas-lizing/src/app/main-container/main-container.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarLeasingCalculatorComponent } from '../components/car-leasing-calculator/car-leasing-calculator.component';
 
 
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarLeasingCalculatorComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
